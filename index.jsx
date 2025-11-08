44c863d591a59f8d6646c3bc5ba3bd023f77097a
unknown <hongdangg.00@gmail.com>
2025-11-08 18:44:36 +0700

    Update project

--- index.jsx
+++ index.jsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // Đảm bảo tệp App.jsx ở cùng thư mục
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
