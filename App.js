91123f03507fb9d39dddf69eefc153b89e4f5262
hongdangg <hongdangg.00@gmail.com>
2025-11-17 05:39:06 +0700

    Update App.js

--- App.js
+++ App.js
@@ -38,7 +38,6 @@ const NAME_PROMPTS = [
     "Tên bạn là gì? Mình tò mò lắm rồi!",
     "Chúng mình làm quen nhé! Bạn tên gì?",
     "Cho xin tên để mình còn simp nha!",
-    "Chà, đẹp trai thế này chắc tên cũng chất! Cho xin với!",
     "Đẹp trai quá! Cho xin tên để tớ còn kịp thả tim!",
     "Nụ cười tỏa nắng quá! Tên của bạn là gì vậy?",
     "Ôi visual crush rồi! Cho xin tên để còn về mơ!",
